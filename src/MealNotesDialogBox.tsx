1231bed15fb7e69d670e7a905abcef14012cb7b8
Søren Anton Smidt <au754363@uni.au.dk>
2025-10-07 10:29:08 +0200

    NotesDialogBox is now a popup, and it closes when a meal is added.

--- src/MealNotesDialogBox.tsx
+++ src/MealNotesDialogBox.tsx
@@ -14,15 +14,23 @@ export function MealNotesDialogBox({isOpen, onClose, onAddMeal}: MealSelectorPro
     if (!isOpen) return null;
 
     return (
-        <div className="mealNotesDialog">
-            <input
-                type={"text"}
-                placeholder={"Tilføjer noter..."}
-                value={notes}
-                onChange={ (e) => setNotes(e.target.value) }
-            />
-            <button onClick={() => onAddMeal(notes)}>Tilføj ret</button>
-            <button onClick={() => onClose()}>Luk</button>
+        <div className="popup-overlay" onClick={() => onClose()}>
+            <div className="popup-notesDialog" onClick={(e) => e.stopPropagation()}>
+                <div className="mealNotesDialog">
+                    <input
+                        type={"text"}
+                        placeholder={"Tilføjer noter..."}
+                        value={notes}
+                        onChange={ (e) => setNotes(e.target.value) }
+                    />
+                    <button onClick={() =>  {
+                        onClose();
+                        onAddMeal(notes);
+                    }}>Tilføj ret</button>
+                    <button onClick={() => onClose()}>Luk</button>
+                </div>
+            </div>
         </div>
+
     )
 }
